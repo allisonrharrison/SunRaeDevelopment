8c91627cf8085d96419457a5e7b3a87ce485ef80
Allison Harrison <allisonrharrison@gmail.com>
2020-01-28 22:39:16 -0800

    Separate Main Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,114 +1,23 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import Home from "./HomeComponent";
-import About from "./AboutComponent";
 import Projects from "./ProjectsComponent";
+import About from "./AboutComponent";
 import Contact from "./ContactComponent";
-import {
-  TabContent,
-  TabPane,
-  Nav,
-  Navbar,
-  NavbarText,
-  NavItem,
-  NavLink,
-  Card,
-  Button,
-  CardTitle,
-  CardText,
-  Row,
-  Col
-} from "reactstrap";
-import classnames from "classnames";
+import Navigation from "./NavComponent";
 
-const Main = props => {
-  const [activeTab, setActiveTab] = useState("1");
+class Main extends React.Component {
 
-  const toggle = tab => {
-    if (activeTab !== tab) setActiveTab(tab);
-  };
-  return (
-    <div width="90%">
-      <Navbar>
-      <Nav tabs id="tabs">
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === "1" })}
-            onClick={() => {
-              toggle("1");
-            }}
-          >
-            <h6 className="navtabs"><i style={{color: "steelblue"}} className="fas fa-home"></i> home</h6>
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === "2" })}
-            onClick={() => {
-              toggle("2");
-            }}
-          >
-            <h6 className="navtabs"><i style={{color: "steelblue"}} className="fas fa-address-card"></i> about</h6>
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === "3" })}
-            onClick={() => {
-              toggle("3");
-            }}
-          >
-            <h6 className="navtabs"><i style={{color: "steelblue"}} className="fas fa-folder-open"></i> portfolio</h6>
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === "4" })}
-            onClick={() => {
-              toggle("4");
-            }}
-          >
-            <h6 className="navtabs"><i style={{color: "steelblue"}} className="fas fa-envelope"></i> contact</h6>
-          </NavLink>
-        </NavItem>
-        </Nav>
-      <NavbarText>
-        <span id="social">
-            <a
-              className="rainbow"
-              href="https://www.linkedin.com/in/allisonrharrison/"
-              target="_blank"
-              title="LinkedIn"
-            >
-              <i className="fab fa-linkedin fa-2x"></i>
-            </a>
-            &nbsp;
-            <a
-              className="rainbow"
-              href="https://github.com/allisonrharrison"
-              target="_blank"
-              title="GitHub"
-            >
-              <i className="fab fa-github-square fa-2x"></i>
-            </a>
-            </span>
-          </NavbarText>
-          </Navbar>
-      <TabContent activeTab={activeTab}>
-        <TabPane tabId="1">
-          <Home />
-        </TabPane>
-        <TabPane tabId="2">
-          <About />
-        </TabPane>
-        <TabPane tabId="3">
-          <Projects />
-        </TabPane>
-        <TabPane tabId="4">
-          <Contact />
-        </TabPane>
-      </TabContent>
-    </div>
-  );
-};
+  render() {
+    return(
+      <div className="col-12">
+      <Navigation />
+      <Home />
+      <Projects />
+      <About />
+      <Contact />
+      </div>
+    )
+  }
+}
 
 export default Main;
---
+++ src/components/NavComponent.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import {
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  NavLink
+} from "reactstrap";
+import { Slide } from "react-reveal";
+
+const Navigation = props => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  return (
+    <div>
+      <Navbar light expand="sm" className="v-nav">
+        <NavbarToggler onClick={toggleNavbar} className="" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav>
+            <NavItem>
+              <NavLink href="#home" className="v-navlink">
+                <h6>
+                  <i
+                    style={{ color: "steelblue" }}
+                    className="fas fa-home fa-sm"
+                  ></i>{" "}
+                  home
+                </h6>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#projects" className="v-navlink">
+                <h6>
+                  <i
+                    style={{ color: "steelblue" }}
+                    className="fas fa-folder-open fa-sm"
+                  ></i>{" "}
+                  projects
+                </h6>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#about" className="v-navlink">
+                <h6>
+                  <i
+                    style={{ color: "steelblue" }}
+                    className="fas fa-address-card fa-sm"
+                  ></i>{" "}
+                  about
+                </h6>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#contact" className="v-navlink">
+                <h6>
+                  <i
+                    style={{ color: "steelblue" }}
+                    className="fas fa-envelope fa-sm"
+                  ></i>{" "}
+                  contact
+                </h6>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                href="https://www.linkedin.com/in/allisonrharrison/"
+                target="_blank"
+                className="v-navlink"
+              >
+                <h6>
+                  <i
+                    style={{ color: "steelblue" }}
+                    className="fab fa-linkedin"
+                  ></i>{" "}
+                  linkedin
+                </h6>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                href="https://github.com/allisonrharrison"
+                target="_blank"
+                className="v-navlink"
+              >
+                <h6>
+                  <i
+                    style={{ color: "steelblue" }}
+                    className="fab fa-github-square"
+                  ></i>{" "}
+                  github
+                </h6>
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navigation;
