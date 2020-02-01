56e2940f3f94a87fd6ac5adcc73d3cc83368105b
Allison Harrison <allisonrharrison@gmail.com>
2020-02-01 01:19:36 -0800

    About

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,15 +1,49 @@
 import React from "react";
+import { Button, ButtonGroup } from "reactstrap";
 import { Fade } from "react-reveal";
 
 function About() {
   return (
-      <div className="panel" id="about">
-        <Fade>
-          <h3>ABOUT</h3>
-          <h3>COMING SOON</h3>
-        </Fade>
-        <hr />
-      </div>
+    <div className="panel" id="about">
+      <Fade>
+        <h3>ABOUT ME</h3>
+        <div className="row mt-5">
+          <div id="imgcontainer" className="col-5">
+            <img id="allison" className="mb-5" src={require("../img/allison.jpeg")} />
+            <br />
+            <ButtonGroup vertical>
+            <Button className="aboutbtn">
+              <i style={{ color: "steelblue" }}
+                    className="fab fa-linkedin mr-2"
+                  ></i>connect on linkedin</Button>
+            <Button className="aboutbtn"><i style={{ color: "steelblue" }}
+                    className="fab fa-github-square mr-2"
+                  ></i>view my github</Button>
+            <Button className="aboutbtn">
+              <i style={{ color: "steelblue" }} className="fas fa-download mr-2"></i>download my resume
+              </Button>
+            </ButtonGroup>
+            </div>
+            <div id="txtcontainer" className="col-7">
+              <p><strong>Hi! I'm Allison.</strong></p>
+              <p>
+                I'm a Web Developer and recent <a href="https://www.nucamp.co/">NuCamp</a> graduate in Tacoma, WA.
+              </p>
+              <p>Current Skills:</p>
+              <ul>
+                <li>HTML5</li>
+                <li>CSS3 | SCSS</li>
+                <li>Bootstrap 4</li>
+                <li>JavaScript ES6 | JSX</li>
+                <li>React | React Native | Redux</li>
+                <li>Git | GitHub</li>
+                <li>NPM | Yarn</li>
+                <li>Visual Studio Code</li>
+              </ul>
+          </div>
+          </div>
+      </Fade>
+    </div>
   );
 }
 
