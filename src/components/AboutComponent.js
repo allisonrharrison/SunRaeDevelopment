70c4606e6526fdba25111490ee182308d0ec00d0
Allison Harrison <allisonrharrison@gmail.com>
2020-02-01 16:21:07 -0800

    Update About

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -9,39 +9,72 @@ function About() {
         <h3>ABOUT ME</h3>
         <div className="row mt-5">
           <div id="imgcontainer" className="col-5">
-            <img id="allison" className="mb-5" src={require("../img/allison.jpeg")} />
+            <img
+              id="allison"
+              className="mb-5"
+              src={require("../img/allison2.jpg")}
+            />
             <br />
             <ButtonGroup vertical>
-            <Button className="aboutbtn">
-              <i style={{ color: "steelblue" }}
-                    className="fab fa-linkedin mr-2"
-                  ></i>connect on linkedin</Button>
-            <Button className="aboutbtn"><i style={{ color: "steelblue" }}
-                    className="fab fa-github-square mr-2"
-                  ></i>view my github</Button>
-            <Button className="aboutbtn">
-              <i style={{ color: "steelblue" }} className="fas fa-download mr-2"></i>download my resume
+              <Button
+                href="https://www.linkedin.com/in/allisonrharrison/"
+                target="_blank"
+                className="aboutbtn"
+              >
+                <i
+                  style={{ color: "steelblue" }}
+                  className="fab fa-linkedin mr-2"
+                ></i>
+                connect on linkedin
+              </Button>
+              <Button
+                href="https://github.com/allisonrharrison"
+                target="_blank"
+                className="aboutbtn"
+              >
+                <i
+                  style={{ color: "steelblue" }}
+                  className="fab fa-github-square mr-2"
+                ></i>
+                view my github
+              </Button>
+              <Button className="aboutbtn">
+                <i
+                  style={{ color: "steelblue" }}
+                  className="fas fa-download mr-2"
+                ></i>
+                download my resume
               </Button>
             </ButtonGroup>
-            </div>
-            <div id="txtcontainer" className="col-7">
-              <p><strong>Hi! I'm Allison.</strong></p>
-              <p>
-                I'm a Web Developer and recent <a href="https://www.nucamp.co/">NuCamp</a> graduate in Tacoma, WA.
-              </p>
-              <p>Current Skills:</p>
-              <ul>
-                <li>HTML5</li>
-                <li>CSS3 | SCSS</li>
-                <li>Bootstrap 4</li>
-                <li>JavaScript ES6 | JSX</li>
-                <li>React | React Native | Redux</li>
-                <li>Git | GitHub</li>
-                <li>NPM | Yarn</li>
-                <li>Visual Studio Code</li>
-              </ul>
           </div>
+          <div id="txtcontainer" className="col-7">
+            <p>
+              <strong>Hi! I'm Allison.</strong>
+            </p>
+            <p>I'm a junior Web Developer in Tacoma, WA.</p>
+            <p>Education:</p>
+            <ul>
+              <li>
+                Full-Stack Web Development (
+                <a href="https://www.nucamp.co/">NuCamp</a>, Jan '20)
+              </li>
+            </ul>
+            <p>Current Skills:</p>
+            <ul>
+              <li>React | React Native | Redux</li>
+              <li>HTML5</li>
+              <li>CSS3 | SCSS</li>
+              <li>JavaScript ES6 | JSX</li>
+              <li>Git | GitHub</li>
+              <li>Bootstrap 4</li>
+              <li>NPM | Yarn</li>
+              <li>Visual Studio Code</li>
+            </ul>
+            <p>
+              Want to add me to your team? <a href="#contact">Contact me</a>.
+            </p>
           </div>
+        </div>
       </Fade>
     </div>
   );
