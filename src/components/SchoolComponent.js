7ca13a0e28234fd19405f73683172c3a393eb9cb
Allison Harrison <allisonrharrison@gmail.com>
2020-02-05 23:50:45 -0800

    Bootstrap Course Project Updated

--- src/components/SchoolComponent.js
+++ src/components/SchoolComponent.js
@@ -12,140 +12,151 @@ import { Fade } from "react-reveal";
 
 class School extends React.Component {
   render() {
-    return(
-        <div className="panel">
-          <Fade>
-        <h3 className="mb-5">SCHOOL PROJECTS</h3>
-      <Card style={{ border: "none" }}>
-        <CardBody>
-          <CardDeck>
+    return (
+      <div className="panel">
+        <Fade>
+          <h3 className="mb-5">SCHOOL PROJECTS</h3>
+          <Card style={{ border: "none" }}>
+            <CardBody>
+              <CardDeck>
+                {/* BOOTSTRAP */}
+                <Card className="text-center proj col-sm-12">
+                  <a
+                    href="https://allisonrharrison.github.io/conFusion/"
+                    target="_blank"
+                  >
+                    <CardImg
+                      top
+                      src={require("../img/bootstrapconfusion-screenshot.png")}
+                      alt="Card image cap"
+                    />
+                  </a>
+                  <CardBody>
+                    <CardTitle>
+                      <h5>bootstrap course</h5>
+                    </CardTitle>
+                    <CardText>
+                      <p>
+                        Webpage for a fictional restaurant with an adaptive
+                        layout for all screen sizes.
+                      </p>
+                      <p>
+                        <strong>Features:</strong> Bootstrap 4, Grunt
+                      </p>
+                    </CardText>
+                    <hr />
+                    <Button
+                      className="aboutbtn"
+                      href="https://allisonrharrison.github.io/conFusion/"
+                      target="_blank"
+                      size="sm"
+                    >
+                      <i className="fas fa-external-link-alt fa-sm"></i> Visit
+                      Site
+                    </Button>
 
-            {/* BOOTSTRAP */}
-            <Card className="text-center proj col-sm-12">
-              <a
-                href="https://github.com/allisonrharrison/conFusion"
-                target="_blank"
-              >
-                <CardImg
-                  top
-                  src={require("../img/bootstrapcourse_screenshot.png")}
-                  alt="Card image cap"
-                />
-              </a>
-              <CardBody>
-                <CardTitle>
-                  <h5>bootstrap site</h5>
-                </CardTitle>
-                <CardText>
-                  <p>
-                    Webpage for a fictional restaurant with an adaptive layout
-                    for all screen sizes.
-                  </p>
-                  <p>
-                    <strong>Features:</strong> Bootstrap 4, Grunt
-                  </p>
-                </CardText>
-                <hr />
-                <Button className="aboutbtn" href="/" target="_blank" size="sm"
-                >
-                  <i className="fas fa-external-link-alt fa-sm"></i> Visit Site
-                </Button>
-                
-                <Button
-                className="aboutbtn"
-                  href="https://github.com/allisonrharrison/MindsEyeTattooStudio"
-                  target="_blank"
-                  size="sm"
-                  
-                >
-                  <i className="fab fa-github"></i> View on GitHub
-                </Button>
-              </CardBody>
-            </Card>
+                    <Button
+                      className="aboutbtn"
+                      href="https://github.com/allisonrharrison/conFusion"
+                      target="_blank"
+                      size="sm"
+                    >
+                      <i className="fab fa-github"></i> View on GitHub
+                    </Button>
+                  </CardBody>
+                </Card>
 
-            {/* REACT */}
-            <Card className="text-center proj col-sm-12">
-              <a
-                href="https://github.com/allisonrharrison/React"
-                target="_blank"
-              >
-                <CardImg
-                  top
-                  src={require("../img/reactcourse_screenshot.png")}
-                  alt="Card image cap"
-                />
-              </a>
-              <CardBody>
-                <CardTitle>
-                  <h5>react app</h5>
-                </CardTitle>
-                <CardText>
-                  <p>
-                    A single-page application for a fictional restaurant with
-                    rating, comment, and favorite functionality.
-                  </p>
-                  <p>
-                    <strong>Features:</strong> React, Redux
-                  </p>
-                </CardText>
-                <hr />
-                <Button className="aboutbtn" href="/" target="_blank" size="sm"
-                >
-                  <i className="fas fa-external-link-alt fa-sm"></i> Visit Site
-                </Button>
-                <Button
-                className="aboutbtn"
-                  href="https://github.com/allisonrharrison/MindsEyeTattooStudio"
-                  target="_blank"
-                  size="sm"
-                  
-                >
-                  <i className="fab fa-github"></i> View on GitHub
-                </Button>
-              </CardBody>
-            </Card>
+                {/* REACT */}
+                <Card className="text-center proj col-sm-12">
+                  <a
+                    href="https://github.com/allisonrharrison/React"
+                    target="_blank"
+                  >
+                    <CardImg
+                      top
+                      src={require("../img/reactcourse_screenshot.png")}
+                      alt="Card image cap"
+                    />
+                  </a>
+                  <CardBody>
+                    <CardTitle>
+                      <h5>react course</h5>
+                    </CardTitle>
+                    <CardText>
+                      <p>
+                        A single-page React application for a fictional restaurant
+                        with rating, comment, and favorite functionality.
+                      </p>
+                      <p>
+                        <strong>Features:</strong> React, Redux
+                      </p>
+                    </CardText>
+                    <hr />
+                    <Button
+                      className="aboutbtn"
+                      href="/"
+                      target="_blank"
+                      size="sm"
+                    >
+                      <i className="fas fa-external-link-alt fa-sm"></i> Visit
+                      Site
+                    </Button>
+                    <Button
+                      className="aboutbtn"
+                      href="https://github.com/allisonrharrison/MindsEyeTattooStudio"
+                      target="_blank"
+                      size="sm"
+                    >
+                      <i className="fab fa-github"></i> View on GitHub
+                    </Button>
+                  </CardBody>
+                </Card>
 
-            {/* REACT NATIVE */}
-            <Card className="text-center proj col-sm-12">
-              <a
-                href="https://github.com/allisonrharrison/NativeConfusion"
-                target="_blank"
-              >
-                <CardImg top src="" alt="Card image cap" />
-              </a>
-              <CardBody>
-                <CardTitle>
-                  <h5>react native app</h5>
-                </CardTitle>
-                <CardText>
-                  <p>
-                    An Android application for a fictional restaurant,
-                    implementing social media sharing, notifications, camera,
-                    and calendar functionality.
-                  </p>
-                  <p>
-                    <strong>Features:</strong> React Native
-                  </p>
-                </CardText>
-                <hr />
-                <Button className="aboutbtn" href="/" target="_blank" size="sm"
-                >
-                  <i className="fas fa-external-link-alt fa-sm"></i> Visit Site
-                </Button>
-                
-                <Button
-                className="aboutbtn"
-                  href="https://github.com/allisonrharrison/MindsEyeTattooStudio"
-                  target="_blank"
-                  size="sm"
-                  
-                >
-                  <i className="fab fa-github"></i> View on GitHub
-                </Button>
-              </CardBody>
-            </Card>
+                {/* REACT NATIVE */}
+                <Card className="text-center proj col-sm-12">
+                  <a
+                    href="https://github.com/allisonrharrison/NativeConfusion"
+                    target="_blank"
+                  >
+                    <CardImg top src="" alt="Card image cap" />
+                  </a>
+                  <CardBody>
+                    <CardTitle>
+                      <h5>react native course</h5>
+                    </CardTitle>
+                    <CardText>
+                      <p>
+                        An Android application for a fictional restaurant,
+                        implementing social media sharing, notifications,
+                        camera, and calendar functionality.
+                      </p>
+                      <p>
+                        <strong>Features:</strong> React Native
+                      </p>
+                    </CardText>
+                    <hr />
+                    <Button
+                      className="aboutbtn"
+                      href="/"
+                      target="_blank"
+                      size="sm"
+                    >
+                      <i className="fas fa-external-link-alt fa-sm"></i> Visit
+                      Site
+                    </Button>
 
-            {/* EXPRESS 
+                    <Button
+                      className="aboutbtn"
+                      href="https://github.com/allisonrharrison/MindsEyeTattooStudio"
+                      target="_blank"
+                      size="sm"
+                    >
+                      <i className="fab fa-github"></i> View on GitHub
+                    </Button>
+                  </CardBody>
+                </Card>
+
+                {/* EXPRESS 
             <Card className="text-center proj col-sm-12">
               <a
                 href="https://github.com/allisonrharrison/conFusionServer"
@@ -181,12 +192,12 @@ class School extends React.Component {
                 </a>
               </CardBody>
             </Card>*/}
-          </CardDeck>
-        </CardBody>
-      </Card>
-      </Fade>
+              </CardDeck>
+            </CardBody>
+          </Card>
+        </Fade>
       </div>
-    )
+    );
   }
 }
 
