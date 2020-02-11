c208bf993eb94abfe972656d956667839d06c66f
Allison Harrison <allisonrharrison@gmail.com>
2020-02-11 13:43:31 -0800

    Contact Component

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {ButtonGroup, Button} from "reactstrap";
 import { Fade } from 'react-reveal';
 
 function Contact() {
@@ -6,7 +7,16 @@ function Contact() {
     <div className="panel" id="contact">
       <Fade>
       <h3>contact me</h3>
-      <h4>coming soon</h4>
+      <div className="d-flex justify-content-center">
+      <ButtonGroup vertical>
+        <Button block outline color="none" href="mailto:admin@sunrae.dev">
+      <h4> <i className="fas fa-envelope fa-lg float-left mr-4"></i> admin@sunrae.dev</h4>
+      </Button>
+      <Button block outline color="none" href="tel:1-253-285-0699">
+      <h4><i className="fas fa-phone fa-lg float-left"></i> (253) 285-0699</h4>
+      </Button>
+      </ButtonGroup>
+      </div>
       </Fade>
       </div>
   )
