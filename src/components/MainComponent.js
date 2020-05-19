e13e94f853ba8851a2820f2369b2909dd675c03f
Allison Harrison <allisonrharrison@gmail.com>
2020-05-18 22:39:10 -0700

    Design Overhaul

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,22 +1,13 @@
 import React, { Component } from "react";
 import Home from "./HomeComponent";
-import Projects from "./ProjectsComponent";
-import About from "./AboutComponent";
-import Contact from "./ContactComponent";
-import Navigation from "./NavComponent";
 
 class Main extends Component {
-
   render() {
-    return(
+    return (
       <div className="container">
-      <Navigation />
-      <Home />
-      <About />
-      <Projects />
-      <Contact />
+        <Home />
       </div>
-    )
+    );
   }
 }
 
