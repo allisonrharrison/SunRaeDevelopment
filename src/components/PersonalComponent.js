b5617bf8d6aa37ae9f5a13adf02aca54605961f3
Allison Harrison <allisonrharrison@gmail.com>
2020-05-29 23:25:23 -0700

    Condense Project Components

--- src/components/PersonalComponent.js
+++ src/components/PersonalComponent.js
@@ -14,14 +14,12 @@ import { Fade } from "react-reveal";
 class Personal extends React.Component {
   render() {
     return (
-      <div className="panel">
+      <div className="mb-5">
         <Fade>
-          <Card style={{ border: "none" }}>
-            <CardTitle>
+          <div>
               <h3 className="mb-5">PERSONAL PROJECTS</h3>
-            </CardTitle>
             <CardBody>
-              <CardDeck>
+              <CardDeck className="row mb-5">
                 {/* MIND'S EYE TATTOO STUDIO */}
                 <Card className="text-center col-sm-12 proj p-0">
                   <a
@@ -34,11 +32,12 @@ class Personal extends React.Component {
                       alt="Card image cap"
                       height="180px"
                       width="318px"
+                      className="cardimg"
                     />
                   </a>
                   <CardBody>
                     <CardTitle>
-                      <h5>mind's eye tattoo</h5>
+                      <h6>mind's eye tattoo</h6>
                     </CardTitle>
                     <CardText className="mt-3">
                       <p>A website for a local tattoo business.</p>
@@ -78,11 +77,12 @@ class Personal extends React.Component {
                       alt="Card image cap"
                       height="180px"
                       width="318px"
+                      className="cardimg"
                     />
                   </a>
                   <CardBody>
                     <CardTitle>
-                      <h5>ilnp</h5>
+                      <h6>ilnp</h6>
                     </CardTitle>
                     <CardText className="mt-3">
                       <p>
@@ -122,11 +122,12 @@ class Personal extends React.Component {
                       alt="Card image cap"
                       height="180px"
                       width="318px"
+                      className="cardimg"
                     />
                   </a>
                   <CardBody>
                     <CardTitle>
-                      <h5>sun rae development</h5>
+                      <h6>sun rae development</h6>
                     </CardTitle>
                     <CardText className="mt-3">
                       <p>
@@ -151,9 +152,119 @@ class Personal extends React.Component {
                     </Button>
                   </CardBody>
                 </Card>
+                </CardDeck>
+                <CardDeck>
+                <Card className="card text-center proj col-sm-12 p-0">
+              <a
+                href="https://codepen.io/allisonrharrison/pen/ExYXJoe"
+                target="_blank"
+              >
+                <CardImg
+                  top
+                  src={require("../img/leapyear_screenshot.png")}
+                  alt="Card image cap"
+                  height="180px" width="318px"
+                />
+              </a>
+              <CardBody>
+                <CardTitle>
+                  <h6>leap year calculator</h6>
+                </CardTitle>
+                <CardText className="mt-2">
+                  <p className="card-text">
+                    A basic app which will take in any year & calculate if it is
+                    a Leap Year.
+                  </p>
+                  <p className="card-text">
+                    <strong>Features:</strong> HTML, CSS, JavaScript
+                  </p>
+                </CardText>
+                <hr />
+                <Button
+                className="aboutbtn"
+                  href="https://codepen.io/allisonrharrison/pen/ExYXJoe"
+                  target="_blank"
+                  size="sm"
+                >
+                  <i className="fab fa-codepen"></i> Demo on CodePen
+                </Button>
+              </CardBody>
+            </Card>
+            <Card className="text-center proj col-sm-12 p-0">
+              <a
+                href="https://codepen.io/allisonrharrison/pen/PoYjgRZ"
+                target="_blank"
+              >
+                <CardImg
+                  top
+                  src={require("../img/eightball_cardsize.png")}
+                  alt="Card image cap"
+                  height="180px" width="318px"
+                />
+              </a>
+              <CardBody>
+                <CardTitle>
+                  <h6>magic eight ball</h6>
+                </CardTitle>
+                <CardText className="mt-2">
+                  <p>
+                    Input a question & the app will randomly generate your
+                    fortune.
+                  </p>
+                  <p>
+                    <strong>Features:</strong> HTML, CSS, JavaScript
+                  </p>
+                </CardText>
+                <hr />
+                <Button
+                className="aboutbtn"
+                  href="https://codepen.io/allisonrharrison/pen/PoYjgRZ"
+                  target="_blank"
+                  size="sm"
+                >
+                  <i className="fab fa-codepen"></i> Demo on CodePen
+                </Button>
+              </CardBody>
+            </Card>
+            <Card className="text-center proj col-sm-12 p-0">
+              <a
+                href="https://codepen.io/allisonrharrison/pen/WNbVOYL"
+                target="_blank"
+              >
+                <CardImg
+                  top
+                  src={require("../img/rockpaperscissors_cardsize.png")}
+                  alt="Card image cap"
+                  height="180px" width="318px"
+                />
+              </a>
+              <CardBody>
+                <CardTitle>
+                  <h6>rock paper scissors</h6>
+                </CardTitle>
+                <CardText className="mt-2">
+                  <p>
+                  Select Rock, Paper, or Scissors on the left-hand side
+and try to beat the Computer.
+                  </p>
+                </CardText>
+                <p>
+                  <strong>Features:</strong> HTML, CSS, JavaScript
+                </p>
+                <hr />
+                <Button
+                className="aboutbtn"
+                  href="https://codepen.io/allisonrharrison/pen/WNbVOYL"
+                  target="_blank"
+                  size="sm"
+                >
+                  <i className="fab fa-codepen"></i> Demo on CodePen
+                </Button>
+              </CardBody>
+            </Card>
               </CardDeck>
             </CardBody>
-          </Card>
+          </div>
         </Fade>
       </div>
     );
