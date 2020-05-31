a9d741c69b3d4e54b8d7fe29f49a00bd23451955
Allison Harrison <allisonrharrison@gmail.com>
2020-05-30 17:27:41 -0700

    Replace Divs with Reactstrap elements

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,21 +1,21 @@
 import React from "react";
-import {ButtonGroup, Button} from "reactstrap";
+import {ButtonGroup, Button, Container, Row, Col} from "reactstrap";
 import { Fade } from 'react-reveal';
 
 function Contact() {
   return (
-    <div className="pb-5" id="contact">
+    <Container className="pb-5" id="contact">
       <Fade>
       <h3 className="mb-5">contact me</h3>
-      <div className="d-flex justify-content-center row">
+      <Row className="d-flex justify-content-center">
       <a href="mailto:admin@sunrae.dev"> <i className="fas fa-envelope fa-lg float-left mr-4"></i>ADMIN@SUNRAE.DEV</a>
-      </div>
+      </Row>
       <br />
-      <div className="d-flex justify-content-center row">
+      <Row className="d-flex justify-content-center">
       <a href="tel:1-253-285-0699"><i className="fas fa-phone fa-lg float-left mr-4"></i>(253) 285-0699</a>
-      </div>
+      </Row>
       </Fade>
-      </div>
+      </Container>
   )
 }
 
