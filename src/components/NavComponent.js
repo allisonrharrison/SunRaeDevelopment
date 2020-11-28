c5bcea590391c21a3f6e2bba2ea595950227dbaf
Allison Harrison <allisonrharrison@gmail.com>
2020-11-27 21:20:31 -0800

    Navbar styling

--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -1,110 +1,73 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
-  Nav,
-  Navbar,
-  NavbarBrand,
-  NavbarToggler,
+  Container,
   Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
   NavItem,
-  NavLink
-} from "reactstrap";
-import { Slide } from "react-reveal";
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
 
-const Navigation = props => {
-  const [collapsed, setCollapsed] = useState(true);
+export const Navigation = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleNavbar = () => setCollapsed(!collapsed);
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div className="container">
-    <Navbar light expand="sm" className="v-nav">
-      <NavbarToggler onClick={toggleNavbar} className="" />
-      <Collapse isOpen={!collapsed} navbar>
-        <Nav>
-        <NavbarBrand className="float-sm-right" id="navbarbrand" href="/">
-        <h4>
-          <i className="fas fa-cloud-sun" style={{color: "steelblue"}}></i> <span  id="navbarlogo">sunrae.dev</span>
-        </h4>
-      </NavbarBrand>
+    <div className="container-fluid">
+      <Navbar dark expand="md">
+        <NavbarBrand id="navbarbrand" href="/">SUNRAE.DEV</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav navbar id="the-nav">
           <NavItem>
-            <NavLink href="/" className="v-navlink">
-              <h6>
-                <i
-                  style={{color: "#6597c1"}}
-                  className="fas fa-home fa-sm mr-2"
-                ></i>
-                home
-              </h6>
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#about" className="v-navlink">
-              <h6>
-                <i
-                  style={{color: "#6597c1"}}
-                  className="fas fa-address-card fa-sm mr-2"
-                ></i>
-                about
-              </h6>
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#projects" className="v-navlink">
-              <h6>
-                <i
-                  style={{color: "#6597c1"}}
-                  className="fas fa-folder-open fa-sm mr-2"
-                ></i>
-                projects
-              </h6>
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#contact" className="v-navlink">
-              <h6>
-                <i
-                  style={{color: "#6597c1"}}
-                  className="fas fa-envelope fa-sm mr-2"
-                ></i>
-                contact
-              </h6>
+            <NavLink className="navlink" href="#about-me">
+              ABOUT ME
             </NavLink>
           </NavItem>
           <NavItem>
             <NavLink
-              href="https://www.linkedin.com/in/allisonrharrison/"
-              target="_blank"
-              className="v-navlink"
+              className="navlink"
+              href="#web-projects"
             >
-              <h6>
-                <i
-                  style={{color: "#6597c1"}}
-                  className="fab fa-linkedin mr-2"
-                ></i>
-                linkedin
-              </h6>
+              WEB PROJECTS
             </NavLink>
           </NavItem>
           <NavItem>
             <NavLink
-              href="https://github.com/allisonrharrison"
-              target="_blank"
-              className="v-navlink"
+              className="navlink"
+              href="#games-apps"
             >
-              <h6>
-                <i
-                  style={{color: "#6597c1"}}
-                  className="fab fa-github-square mr-2"
-                ></i>
-                github
-              </h6>
+              GAMES & APPS
             </NavLink>
           </NavItem>
-        </Nav>
-      </Collapse>
-    </Navbar>
+          <NavItem>
+            <NavLink
+              className="navlink"
+              href="#school-projects"
+            >
+              SCHOOL PROJECTS
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className="navlink"
+              href="#contact"
+            >
+              CONTACT ME
+            </NavLink>
+          </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
   );
-};
+}
 
 export default Navigation;
